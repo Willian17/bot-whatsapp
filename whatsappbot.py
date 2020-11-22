0637c04dc9681aaf6fe33545d995445f6540f47a
Willian17 <willianfernandes113@gmail.com>
2020-11-22 14:17:11 -0400

    init

---
+++ whatsappbot.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get('https://web.whatsapp.com/')
+time.sleep(30)
+
+contatos = ['Anotações', 'Comunismo']
+mensagem = 'salve'
+
+def buscar_contato(contato):
+    campo_pesquisa = driver.find_element_by_xpath(
+        '//div[contains(@class,"copyable-text selectable-text")]')
+    campo_pesquisa.click()
+    campo_pesquisa.send_keys(contato)
+    campo_pesquisa.send_keys(Keys.ENTER)
+
+def mandar_mensagem(mensagem):
+    campo_mensagem = driver.find_elements_by_xpath('//div[contains(@class, "copyable-text selectable-text")]')
+    campo_mensagem[1].click()
+    campo_mensagem[1].send_keys(mensagem)
+    campo_mensagem[1].send_keys(Keys.ENTER)
+
+
+for contato in contatos:
+    buscar_contato(contato)
+    mandar_mensagem(mensagem)
+
+#copyable-text selectable-text
